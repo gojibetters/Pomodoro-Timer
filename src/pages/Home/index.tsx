91ce95e4ecda8329652ed6bedb95f0fad8a1861a
Bruno Patez <patezbr@gmail.com>
2023-04-10 19:40:29 -0300

    update: home finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,60 @@
 import { Play } from 'phosphor-react'
-import { CountdownContainer, HomeContainer, Separator } from './styles'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
-      <form id="pomodoroForm" action="/">
-        <label htmlFor="task">Vou trabalhar em</label>
-        <input id="task" type="text" />
+      <form action="/">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            type="text"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
 
-        <label htmlFor="minutesAmount">durante</label>
-        <input id="minutesAmount" type="number" />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Laranja" />
+          </datalist>
 
-        <span>minutos.</span>
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
       </form>
-
-      <CountdownContainer>
-        <span>0</span>
-        <span>0</span>
-        <Separator>:</Separator>
-        <span>0</span>
-        <span>0</span>
-      </CountdownContainer>
-
-      <button type="submit" form="pomodoroForm">
-        <Play size={24} />
-        Começar
-      </button>
     </HomeContainer>
   )
 }
